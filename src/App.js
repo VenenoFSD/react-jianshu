6c00ed2ac81f483dbfd00d4ced5d114773f9ea13
VenenoFSD <venenofsd@qq.com>
2019-02-14 10:12:15 +0800

    configure router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { Provider } from 'react-redux'
 import { GlobalStyle } from './style'
 import store from './store'
 import Header from './common/header'
+import { BrowserRouter , Route } from 'react-router-dom'
+import Home from './pages/home'
+import Detail from './pages/detail'
 
 class App extends Component {
   render () {
@@ -10,6 +13,12 @@ class App extends Component {
       <Provider store={store}>
         <GlobalStyle/>
         <Header/>
+        <BrowserRouter>
+          <div>
+            <Route path='/' exact component={Home}/>
+            <Route path='/detail' exact component={Detail}/>
+          </div>
+        </BrowserRouter>
       </Provider>
     );
   }
