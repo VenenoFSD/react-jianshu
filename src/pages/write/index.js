83520f17c409d251d54145b798b4647d7b2414b6
VenenoFSD <venenofsd@qq.com>
2019-02-16 00:31:06 +0800

    test write page

---
+++ src/pages/write/index.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import { connect } from "react-redux"
+import { Redirect } from "react-router-dom"
+
+class Write extends Component {
+
+  render () {
+    return this.props.loginStatus ? (
+      <div>
+        Article
+      </div>
+    ) : (
+      <Redirect to='/login'/>
+    );
+  }
+
+}
+
+const mapState = state => ({
+  loginStatus: state.getIn(['login', 'loginStatus'])
+});
+
+export default connect(mapState, null)(Write)
